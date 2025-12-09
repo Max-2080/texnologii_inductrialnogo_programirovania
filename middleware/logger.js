39c5987a87966464cc856fe93c2b5f27b1f7e97d
BARS-2080 <75639270+Max-2080@users.noreply.github.com>
2025-12-09 16:51:00 +0300

    Create logger.js

---
+++ middleware/logger.js
@@ -0,0 +1,16 @@
+const logger = (req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(`${timestamp} - ${req.method} ${req.url}`);
+  
+  if (Object.keys(req.body).length > 0) {
+    console.log('Тело запроса:', req.body);
+  }
+  
+  if (Object.keys(req.query).length > 0) {
+    console.log('Query параметры:', req.query);
+  }
+  
+  next();
+};
+
+module.exports = logger;
